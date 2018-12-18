426c4054830dda7c63cf47f1516671ef2b7baa76
aamorozov <andreym@hzdg.com>
2018-12-18 00:57:22 -0500

    Adding Stack

---
+++ src/Stack.ts
@@ -0,0 +1,27 @@
+export default class Stack {
+  count: number = 0;
+  storage: { [key: number]: number | string } = {};
+  // Adds a value onto the end of the stack
+  push(value: number | string): void {
+    this.storage[this.count] = value;
+    this.count++;
+  }
+  // Removes and returns the value at the end of the stack
+  pop(): any {
+    if (this.count === 0) {
+      return undefined;
+    }
+    this.count--;
+    const result = this.storage[this.count];
+    delete this.storage[this.count];
+    return result;
+  }
+
+  size(): number {
+    return this.count;
+  }
+  // Returns the value at the end of the stack
+  peek(): any {
+    return this.storage[this.count - 1];
+  }
+}
